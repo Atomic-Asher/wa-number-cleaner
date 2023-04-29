4860b08ed7caec97c56875aa8db89dc3f7fa412e
anirudhasher <75933833+anirudhasher@users.noreply.github.com>
2023-04-29 13:54:44 +0530

    Update renderer.js
    added functionality to look for 0 at start of number
    added funtionality to look for +91 also at the start

--- renderer.js
+++ renderer.js
@@ -1,7 +1,17 @@
 function cleanNumber() {
-  let phoneNumber = document.getElementById("phone").value;
-  phoneNumber = phoneNumber.replace(/\D/g, ""); // remove all non-digit characters
-  phoneNumber = '91' + phoneNumber; // prepend with country code
-  let link = 'https://wa.me/' + phoneNumber;
-  document.getElementById("result").innerHTML = `<a href="${link}" target="_blank">${link}</a>`;
+    let phoneNumber = document.getElementById("phone").value;
+    phoneNumber = phoneNumber.replace(/\D/g, "");  // remove all non-digit characters
+
+    // Remove leading zero if present
+    if (phoneNumber.startsWith("0")) {
+        phoneNumber = phoneNumber.substring(1);
+    }
+
+    // Prepend 91 only if it's not already present at the beginning
+    if (!phoneNumber.startsWith("91")) {
+        phoneNumber = "91" + phoneNumber;
+    }
+
+    let link = "https://wa.me/" + phoneNumber;
+    document.getElementById("result").innerHTML = `<a href="${link}" target="_blank">${link}</a>`;
 }
