713c8da48ee6ef2dee44d20135c4ce78ec75704d
drstone-genius04 <82304884+drstone-genius04@users.noreply.github.com>
2023-05-06 18:57:16 +0530

    Update renderer.js
    updated code logic

--- renderer.js
+++ renderer.js
@@ -1,45 +1,43 @@
 function cleanNumber() {
-  let phoneNumber = document.getElementById("phone").value;
-  let originalNumber = phoneNumber;
-  phoneNumber = phoneNumber.replace(/\D/g, "");  // remove all non-digit characters
-
-  // Remove leading zeros if present
-  let zerosRemoved = 0;
-  while (phoneNumber.startsWith("0")) {
-      phoneNumber = phoneNumber.substring(1);
-      zerosRemoved++;
-  }
-
-  let countryCodeAdded = false;
-  let countryCode = document.getElementById("country").value;
+    let phoneNumber = document.getElementById("phone").value;
+    let originalNumber = phoneNumber;
+    phoneNumber = phoneNumber.replace(/\D/g, "");  // remove all non-digit characters
   
-  if (phoneNumber.length === 10) {
+    // Remove leading zeros if present
+    let zerosRemoved = 0;
+    while (phoneNumber.startsWith("0")) {
+        phoneNumber = phoneNumber.substring(1);
+        zerosRemoved++;
+    }
+  
+    let countryCodeAdded = false;
+    let countryCode = document.getElementById("country").value;
+  
+    // Check if the first two digits match the country code
+    if (phoneNumber.startsWith(countryCode)) {
+      countryCodeAdded = true;
+    } else if (phoneNumber.length === 10) {
       // Prepend country code if it's not already present at the beginning
-      if (!phoneNumber.startsWith(countryCode)) {
-          phoneNumber = countryCode + phoneNumber;
-          countryCodeAdded = true;
-      }
-  } else if (phoneNumber.length === 12 && phoneNumber.startsWith(countryCode)) {
-      phoneNumber = phoneNumber.substring(2);
-  } else {
+      phoneNumber = countryCode + phoneNumber;
+      countryCodeAdded = true;
+    } else if (phoneNumber.length === 12 && phoneNumber.startsWith("00" + countryCode)) {
+      // Remove the first two digits and prepend country code
+      phoneNumber = countryCode + phoneNumber.substring(2);
+      countryCodeAdded = true;
+    } else {
       // Throw an error if the entered number is neither 10 digits nor a 12-digit number with matching country code
       document.getElementById("error").innerHTML = "Invalid phone number";
       return;
+    }
+  
+    let link = "https://wa.me/" + phoneNumber;
+    document.getElementById("result").innerHTML = `<a href="${link}" id="waLink" target="_blank">${link}</a> <button onclick="copyToClipboard()" class="btn btn-secondary">Clipboard</button>`;
+    
+    let changes = "Original number: " + originalNumber + "<br>";
+    changes += "Non-digit characters removed: " + (originalNumber.length - phoneNumber.length + zerosRemoved) + "<br>";
+    changes += "Leading zeros removed: " + zerosRemoved + "<br>";
+    changes += "Country code " + countryCode + " " + (countryCodeAdded ? "added" : "already present");
+    document.getElementById("changes").innerHTML = changes;
+    
+    document.getElementById("error").innerHTML = "";
   }
-
-  let link = "https://wa.me/" + phoneNumber;
-  document.getElementById("result").innerHTML = `<a href="${link}" id="waLink" target="_blank">${link}</a> <button onclick="copyToClipboard()" class="btn btn-secondary">Clipboard</button>`;
-  
-  let changes = "Original number: " + originalNumber + "<br>";
-  changes += "Non-digit characters removed: " + (originalNumber.length - phoneNumber.length + zerosRemoved) + "<br>";
-  changes += "Leading zeros removed: " + zerosRemoved + "<br>";
-  changes += "Country code " + countryCode + " " + (countryCodeAdded ? "added" : "already present");
-  document.getElementById("changes").innerHTML = changes;
-  
-  document.getElementById("error").innerHTML = "";
-}
-
-function copyToClipboard() {
-  let link = document.getElementById("waLink").href;
-  navigator.clipboard.writeText(link);
-}
